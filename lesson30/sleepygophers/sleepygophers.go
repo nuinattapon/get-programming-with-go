36113ca0ffd303ad3a56641f41a61f04cf0ab692
Nattapon Sub-Anake <nattapon.subanake@gmail.com>
2021-05-01 12:04:28 +0700

    Using wg.Add and wg.Done instead of time.Sleep

--- lesson30/sleepygophers/sleepygophers.go
+++ lesson30/sleepygophers/sleepygophers.go
@@ -3,16 +3,23 @@ package main
 import (
 	"fmt"
 	"time"
+	"sync"
+
 )
+
+var wg = sync.WaitGroup{}
 
 func main() {
 	for i := 0; i < 5; i++ {
+		wg.Add(1)
 		go sleepyGopher()
 	}
-	time.Sleep(4 * time.Second)
+	wg.Wait()
 }
 
 func sleepyGopher() {
 	time.Sleep(3 * time.Second)
 	fmt.Println("... snore ...")
+	wg.Done()
+
 }
