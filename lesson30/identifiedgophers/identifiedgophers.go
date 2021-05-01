ca1efd1aa66d3cde2cbd5dc500cf3855b67266eb
Nattapon Sub-Anake <nattapon.subanake@gmail.com>
2021-05-01 12:14:37 +0700

    Use WaitGroup and add random sleep time for each goroutine

--- lesson30/identifiedgophers/identifiedgophers.go
+++ lesson30/identifiedgophers/identifiedgophers.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
 	"time"
 )
 
+var wg = sync.WaitGroup{}
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
 	for i := 0; i < 5; i++ {
+		wg.Add(1)
 		go sleepyGopher(i)
 	}
-	time.Sleep(4 * time.Second)
+	// time.Sleep(4 * time.Second)
+	wg.Wait()
 }
 
 func sleepyGopher(id int) {
-	time.Sleep(3 * time.Second)
-	fmt.Println("... ", id, " snore ...")
+	sleepSecond := rand.Intn(3)
+	time.Sleep(time.Duration(sleepSecond) * time.Second)
+	fmt.Println("... ", id, " snore ...", "sleep", sleepSecond)
+	wg.Done()
 }
